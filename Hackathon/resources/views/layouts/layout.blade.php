9f303bcbea623a56da15b599f030114691183448
TaufanFadhilah <tfi_23@ymail.com>
2017-11-04 09:07:27 +0700

    First init

---
+++ Hackathon/resources/views/layouts/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="{{asset('css/materialize.min.css')}}"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      @stack('css')
+    </head>
+
+    <body>
+      @yield('section')
+      <h1 class="green-text center">Semangat!</h1>
+
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="{{asset('js/materialize.min.js')}}"></script>
+      @stack('js')
+    </body>
+  </html>
